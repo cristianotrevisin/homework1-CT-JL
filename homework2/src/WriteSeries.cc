34fb78e0859d0623c24e89552a6d055a1cdc93ff
chadi <joseph.lemaitre@epfl.ch>
2020-10-28 20:00:09 +0100

    restored compatibility with column headers assumed by plot.py

--- homework2/src/WriteSeries.cc
+++ homework2/src/WriteSeries.cc
@@ -16,7 +16,7 @@ void WriteSeries::dump(std::ostream &os) {
 
     double sol_ana = this->series.getAnalyticPrediction();
 
-
+    os << "iter" << separator << "result" << separator << "error" << std::endl;   
     for (int i = 1; i <= this->maxiter; i += this->freq) {
         double res = this->series.compute(i); 
         os << i << separator << std::setprecision(this->precision) << res << separator << abs(res-sol_ana) << std::endl;   
