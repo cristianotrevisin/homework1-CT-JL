4e6091ca0bcc2e52be0d06feb7fcada60fb52f44
cristianotrevisin <cristianotrevisin@users.noreply.github.com>
2020-10-28 17:18:51 +0100

    Added docstrings to the plotter file

--- homework2/src/plot.py
+++ homework2/src/plot.py
@@ -1,10 +1,32 @@
+""" 
+Plot routines as part of Homework 2 by Cristiano Trevisin and Joseph Lemaitre.
+"""
+
 import matplotlib.pyplot as plt
 import argparse
 import pandas as pd
 import numpy as np
 
 def main():
+    """
+    Prepare the environment for creating the plot of the series.
     
+    Loads the series as a dataframe and detect automatically iteration, result
+    and error. If there are null values in the error column (i.e. because the 
+    analytic prediction was not available in the C++ routines), it only gives 
+    the plot of the series.
+    
+    Takes as arguments: 
+        filename of the series calculations; 
+        desidered separator in such file;
+        output file wanted (if none, it just shows the plot).
+    
+
+    Returns
+    -------
+    None.
+
+    """
     parser = argparse.ArgumentParser(
         description='Plotter of the series convergence')
     parser.add_argument('-f', '--file_name', type=str,
@@ -22,6 +44,7 @@ def main():
     separator = args.separator
     output_file = args.output_file
     
+    # Loads the dataframe
     if separator == 'space':
         data_plot = pd.read_csv(file_name, sep= ' ')
     elif separator == 'tab':
@@ -31,6 +54,7 @@ def main():
     elif separator == 'pipe':
         data_plot = pd.read_csv(file_name, header=True, sep= '|')
     
+    # Plots the dataframe
     if data_plot['error'].isnull().values.any():
         fig, axs = plt.subplots(1,1, squeeze=True)
         axs.plot(data_plot['iter'], data_plot['result'])
@@ -50,13 +74,11 @@ def main():
         axs[1].set_xlabel('Iteration')
         axs[1].grid(True)
         
+    # Decision whether to save or show the plot.
     if output_file == ' ':
         plt.show()
     else:
         plt.savefig(output_file)
           
-    
-    
-    
 if __name__ == "__main__":
     main()
