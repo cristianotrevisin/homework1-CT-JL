eae92b8e41de8c8fb0f39cff92afa0047895431e
cristianotrevisin <cristianotrevisin@users.noreply.github.com>
2020-10-28 17:11:36 +0100

    Add plotter file in python (exercise 3.8)

---
+++ homework2/src/plot.py
@@ -0,0 +1,62 @@
+import matplotlib.pyplot as plt
+import argparse
+import pandas as pd
+import numpy as np
+
+def main():
+    
+    parser = argparse.ArgumentParser(
+        description='Plotter of the series convergence')
+    parser.add_argument('-f', '--file_name', type=str,
+                        help=('file_name where convergence results are stored'), 
+                        default = 'example.txt')
+    parser.add_argument('-s', '--separator', type=str,
+                        help=('separator provided in the file where convergence results are stored'),
+                        default = 'space')
+    parser.add_argument('-o', '--output_file', type=str,
+                        help=('file where the plot is saved'),
+                        default = ' ')
+    
+    args = parser.parse_args()
+    file_name = args.file_name
+    separator = args.separator
+    output_file = args.output_file
+    
+    if separator == 'space':
+        data_plot = pd.read_csv(file_name, sep= ' ')
+    elif separator == 'tab':
+        data_plot = pd.read_csv(file_name, header=True, sep= '\t')
+    elif separator == 'comma':
+        data_plot = pd.read_csv(file_name, header=True, sep= ',')
+    elif separator == 'pipe':
+        data_plot = pd.read_csv(file_name, header=True, sep= '|')
+    
+    if data_plot['error'].isnull().values.any():
+        fig, axs = plt.subplots(1,1, squeeze=True)
+        axs.plot(data_plot['iter'], data_plot['result'])
+        axs.set_xlabel('Iteration')
+        axs.set_ylabel('Series result')
+        axs.legend(["Series"],loc=4)
+        axs.grid(True)
+    else:
+        fig, axs = plt.subplots(2,1,sharex=True, squeeze=True)
+        axs[0].plot(data_plot['iter'], data_plot['result'])
+        axs[0].hlines(np.pi, 0, data_plot.shape[0], 'r', 'dashed')
+        axs[0].set_ylabel('Series result')
+        axs[0].grid(True)
+        axs[0].legend(["Series", "Exact solution"],loc=4)
+        axs[1].plot(data_plot['iter'], data_plot['error'], 'red')
+        axs[1].set_ylabel('Series error')
+        axs[1].set_xlabel('Iteration')
+        axs[1].grid(True)
+        
+    if output_file == ' ':
+        plt.show()
+    else:
+        plt.savefig(output_file)
+          
+    
+    
+    
+if __name__ == "__main__":
+    main()
