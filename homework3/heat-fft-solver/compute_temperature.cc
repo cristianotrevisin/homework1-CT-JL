5e26e054d931753beca9d0c5df0abd0eac7c0915
chadi <joseph.lemaitre@epfl.ch>
2020-12-07 16:11:50 +0100

    first test of reading to compute

--- homework3/heat-fft-solver/compute_temperature.cc
+++ homework3/heat-fft-solver/compute_temperature.cc
@@ -5,6 +5,29 @@
 
 /* -------------------------------------------------------------------------- */
 
-void ComputeTemperature::compute(System& system) {}
+void ComputeTemperature::compute(System& system) {
+
+    int size = sqrt(system.getNbParticles());
+    std::cout << size << std::endl;
+
+    /* Compute one step of the temperature */
+    Matrix<complex> tetha;
+    Matrix<complex> hv;
+
+    for (auto& par : system) {
+          MaterialPoint & matpt = dynamic_cast<MaterialPoint&>(par);
+          std::cout << matpt.getTemperature() << " "
+          << matpt.getHeatRate()<< " -> " << matpt.getPosition() << "-" << matpt.getVelocity() <<
+          matpt.getForce() << matpt.getMass() <<  std::endl;
+    } // end for loop over particles
+
+    /* Create a matrix from the particles */
+    
+    //Matrix<complex> m(N); 
+
+
+
+
+}
 
 /* -------------------------------------------------------------------------- */
