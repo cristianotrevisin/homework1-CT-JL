d604d6d4b49892054b3c7cd8190b6742bb44252b
cristianotrevisin <cristianotrevisin@users.noreply.github.com>
2021-01-06 14:33:19 +0100

    Adding starting point

---
+++ homework4/particles-pybind/pypart.cc
@@ -0,0 +1,18 @@
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+#include "compute_gravity.hh"
+#include "compute_temperature.hh"
+#include "compute_verlet_integration.hh"
+#include "csv_writer.hh"
+#include "material_points_factory.hh"
+#include "ping_pong_balls_factory.hh"
+#include "planets_factory.hh"
+
+PYBIND11_MODULE(pypart, m) {
+
+  m.doc() = "pybind of the Particles project";
+
+  // bind the routines here
+}
