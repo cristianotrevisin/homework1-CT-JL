c7af8a991ef674b0e21e9af3baa8f66a9404ae9f
cristianotrevisin <cristianotrevisin@users.noreply.github.com>
2021-01-14 12:02:12 +0100

    Trajectories and first pybind

---
+++ homework4/particles-pybind/build/CMakeFiles/test_fft.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_fft.dir/test_fft.cc.o"
+  "test_fft"
+  "test_fft.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_fft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ homework4/particles-pybind/build/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/cristiano/Documents/GitHub/homework1-CT-JL/homework4/particles-pybind/googletest
+# Build directory: /Users/cristiano/Documents/GitHub/homework1-CT-JL/homework4/particles-pybind/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
