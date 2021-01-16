fe77220cf94fa3c42233fc84cc58822308eb3fe7
chadi <joseph.lemaitre@epfl.ch>
2021-01-16 14:45:55 +0100

    first unsuccessful try with overloaded

--- homework4/particles-pybind/pypart.cc
+++ homework4/particles-pybind/pypart.cc
@@ -12,7 +12,52 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(pypart, m) {
 
-  m.doc() = "pybind of the Particles project";
+  m.doc() = "pybind of the Particles project, submission of Cristiano Trevisin & Joseph Lemaitre";
 
   // bind the routines here
+  py::class_<ParticlesFactoryInterface>(m, "ParticlesFactoryInterface")
+    .def("getInstance",
+        &ParticlesFactoryInterface::getInstance, 
+        py::return_value_policy::reference)
+    .def("createSimulation",
+        py::overload_cast<const std::string &, Real, py::function>(&ParticlesFactoryInterface::createSimulation<py::function>), 
+        py::return_value_policy::reference)
+    .def("createSimulation",
+        py::overload_cast<const std::string &, Real>(&ParticlesFactoryInterface::createSimulation<py::function>), 
+        py::return_value_policy::reference);
+  /*
+  py::class_<MaterialPointsFactory, ParticlesFactoryInterface>(m, "MaterialPointsFactory")
+    .def("getInstance", 
+        &MaterialPointsFactory::getInstance, 
+        py::return_value_policy::reference)
+    .def("createSimulation",
+        py::overload_cast<const std::string &, Real, py::function>(&MaterialPointsFactory::createSimulation),
+        py::return_value_policy::reference)
+    .def("createSimulation",
+        py::overload_cast<const std::string &, Real>(&MaterialPointsFactory::createSimulation),
+        py::return_value_policy::reference);
+  
+  py::class_<PingPongBallsFactory, ParticlesFactoryInterface>(m, "PingPongBallsFactory")
+    .def("getInstance", 
+        &PingPongBallsFactory::getInstance, 
+        py::return_value_policy::reference)
+    .def("createSimulation",
+        py::overload_cast<const std::string &, Real, py::function>(&PingPongBallsFactory::createSimulation), 
+        py::return_value_policy::reference)
+    .def("createSimulation",
+        py::overload_cast<const std::string &, Real>(&PingPongBallsFactory::createSimulation), 
+        py::return_value_policy::reference);
+  
+  py::class_<PlanetsFactory, ParticlesFactoryInterface>(m, "PlanetsFactory")
+    .def("getInstance", 
+        &PlanetsFactory::getInstance, 
+        py::return_value_policy::reference)
+    .def("createSimulation",
+        py::overload_cast<const std::string &, Real, py::function>(&PlanetsFactory::createSimulation), 
+        py::return_value_policy::reference)
+    .def("createSimulation",
+        py::overload_cast<const std::string &, Real>(&PlanetsFactory::createSimulation), 
+        py::return_value_policy::reference);
+*/
+  
 }
