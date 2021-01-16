13e1f0bf1e6606287fb0821efbbe35e551f968e0
chadi <joseph.lemaitre@epfl.ch>
2021-01-16 10:46:58 +0100

    repo cleanup

--- homework4/particles-pybind/build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/cristiano/Documents/GitHub/homework1-CT-JL/homework4/particles-pybind/googletest/googlemock
-# Build directory: /Users/cristiano/Documents/GitHub/homework1-CT-JL/homework4/particles-pybind/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
